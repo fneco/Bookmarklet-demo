448f45efe1abf27be525388f587f5b99a7ace268
fneco <izumi.f.tech@gmail.com>
2025-02-09 16:54:48 +0900

    Bookmarklet.js

---
+++ Bookmarklet.js
@@ -0,0 +1,5 @@
+javascript: (function () {
+  s = document.createElement("script");
+  s.src = "https://raw.githubusercontent.com/fneco/Bookmarklet/main/hello.js";
+  document.body.appendChild(s);
+})();
