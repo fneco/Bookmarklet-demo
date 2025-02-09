34f2b4e47d54c93b4859e2d51a5a7bd7ef2e2b62
fneco <izumi.f.tech@gmail.com>
2025-02-09 16:54:48 +0900

    use local nginx server

---
+++ Bookmarklet.js.js
@@ -0,0 +1,5 @@
+javascript: (function () {
+  s = document.createElement("script");
+  s.src = "http://localhost:8000/hello.js";
+  document.body.appendChild(s);
+})();
---
+++ memo.md
@@ -0,0 +1,5 @@
+# memo
+
+```sh
+docker run --rm --name local-server -p 8000:80 -v .:/usr/share/nginx/html nginx
+```
