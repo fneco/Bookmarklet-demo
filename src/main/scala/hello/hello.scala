ab6bca08dd97a32dadafe9797de365e824acfca4
fneco <izumi.f.tech@gmail.com>
2025-02-22 15:35:49 +0900

    use scalafix

--- src/main/scala/hello/hello.scala
+++ src/main/scala/hello/hello.scala
@@ -1,15 +1,24 @@
 package sample
 
-import org.scalajs.dom
-
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.Promise
 import scala.scalajs.js
-import scala.scalajs.js.Dynamic.global
-import scala.scalajs.js.annotation.*
-
+import scala.scalajs.js.timers.setTimeout
+import scala.util.Random
 @main
 def hello(): Unit =
-  var bodyChildrenCount = dom.document.body.children.length
-  global.alert(s"""
-  hello with scala!
-  body children count: $bodyChildrenCount
-  """)
+  (1 to 5)
+    .map(_ => sayHello(Random.between(1000, 8000)))
+    .foreach(_.foreach(result => println(s"completed $result")))
+
+  println("hello")
+
+def sayHello(num: Int): Future[String] = {
+  val promise = Promise[String]()
+  setTimeout(num) {
+    promise.success(s"after $num")
+  }
+  println(s"called with $num")
+  promise.future
+}
